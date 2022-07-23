28dd7073b288c584e186389d1aae29607b22503a
John Swinbank <john@swinbank.org>
2022-07-23 09:21:26 +0200

    Test everything

--- tests/test_extractor.py
+++ tests/test_extractor.py
@@ -1,44 +1,45 @@
-import matplotlib as mpl  # must be run first! But therefore requires noqa E02 on all other imports
-mpl.use('Agg')
-
+import logging
+import requests
+import time
+from requests.adapters import HTTPAdapter, Retry
 from numpy.testing import run_module_suite  # noqa: E402
 
-from spectractor import parameters  # noqa: E402
-from spectractor.extractor.extractor import Spectractor  # noqa: E402
-from spectractor.logbook import LogBook  # noqa: E402
-from spectractor.config import load_config, apply_rebinning_to_parameters  # noqa: E402
-import os  # noqa: E402
-import sys  # noqa: E402
-import numpy as np  # noqa: E402
-import unittest  # noqa: E402
 
-#URL = "https://simbad.u-strasbg.fr/simbad/sim-script"
-URL = "https://www.google.com/"
-
-def test_with_retry():
-    import logging
-    import requests
-    import time
-    from requests.adapters import HTTPAdapter, Retry
+def test_google_with_retry():
+    URL = "https://www.google.com/"
     session = requests.Session()
     retries = Retry(total=1)
-    session.mount('https://', HTTPAdapter(max_retries=retries))
+    session.mount("https://", HTTPAdapter(max_retries=retries))
     logging.warn(session.get(URL))
-    time.sleep(5*60)
+    time.sleep(5 * 60)
     logging.warn(session.get(URL))
 
 
-def test_without_retry():
-    import logging
-    import requests
-    import time
-    from requests.adapters import HTTPAdapter, Retry
+def test_google_without_retry():
+    URL = "https://www.google.com/"
     session = requests.Session()
     logging.warn(session.get(URL))
-    time.sleep(5*60)
+    time.sleep(5 * 60)
+    logging.warn(session.get(URL))
+
+
+def test_simbad_with_retry():
+    URL = "https://simbad.u-strasbg.fr/simbad/sim-script"
+    session = requests.Session()
+    retries = Retry(total=1)
+    session.mount("https://", HTTPAdapter(max_retries=retries))
+    logging.warn(session.get(URL))
+    time.sleep(5 * 60)
+    logging.warn(session.get(URL))
+
+
+def test_simbad_without_retry():
+    URL = "https://simbad.u-strasbg.fr/simbad/sim-script"
+    session = requests.Session()
+    logging.warn(session.get(URL))
+    time.sleep(5 * 60)
     logging.warn(session.get(URL))
 
 
 if __name__ == "__main__":
-
     run_module_suite()
